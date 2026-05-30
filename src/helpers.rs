260adc4502db9e32f805d72442d6f5c41556fac5
raidelo <raidelosix@gmail.com>
2026-05-30 13:19:19 -0400

    chore: delete unused function

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,22 +1,12 @@
 use std::{fmt::Display, io};
 
-use crate::types::{Axes, Direction, Window};
+use crate::types::{Axes, Window};
 
 pub fn random_pos(window: &Window) -> Axes {
     Axes::new(
         rand::random_range(0..window.width),
         rand::random_range(0..window.height),
     )
-}
-
-pub fn random_direction() -> Direction {
-    match rand::random_range(1..=4) {
-        1 => Direction::Up,
-        2 => Direction::Down,
-        3 => Direction::Left,
-        4 => Direction::Right,
-        _ => unreachable!(),
-    }
 }
 
 pub fn make_width_pair(width: u16) -> u16 {
